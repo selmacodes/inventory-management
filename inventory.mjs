85bbcfbcf2b012c587b3181e782fbc552f955822
Selma <selma-k@hotmail.se>
2025-10-22 14:02:41 +0200

    Add in-memory product array

---
+++ inventory.mjs
@@ -0,0 +1,9 @@
+// En array som representerar lagret
+let products = [
+    { id: 1, name: "Pennor", quantity: 50, price: 2, category: "Kontorsmaterial" },
+    { id: 2, name: "Anteckningsblock", quantity: 30, price: 15, category: "Kontorsmaterial" },
+    { id: 3, name: "Häftapparat", quantity: 10, price: 120, category: "Kontorsmaterial" },
+    { id: 4, name: "Pärm", quantity: 25, price: 35, category: "Kontorsmaterial" },
+    { id: 5, name: "Tuschpennor", quantity: 40, price: 25, category: "Kontorsmaterial" }
+];
+
