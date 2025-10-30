a4d052096e5035ebf62b7dcfa6744637fecb346a
Selma <selma-k@hotmail.se>
2025-10-30 15:25:30 +0100

    feat: add GET /products endpoint

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Inventory API is running');
-})
+// Lager
+let products = [
+    { id: 1, name: "Pennor", quantity: 50, price: 2, category: "Kontorsmaterial" },
+    { id: 2, name: "Anteckningsblock", quantity: 30, price: 15, category: "Kontorsmaterial" },
+    { id: 3, name: "Häftapparat", quantity: 10, price: 120, category: "Kontorsmaterial" },
+    { id: 4, name: "Pärm", quantity: 25, price: 35, category: "Kontorsmaterial" },
+    { id: 5, name: "Tuschpennor", quantity: 40, price: 25, category: "Kontorsmaterial" }
+];
+
+app.get('/products', (req, res) => {
+    res.json(products);
+});
 
 app.listen(3012, () => console.log('Server running on port 3012'));
