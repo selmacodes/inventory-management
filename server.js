ff912fc8c34435d1230a7d5611494075c66aa46f
Selma <selma-k@hotmail.se>
2025-10-30 15:21:01 +0100

    feat: add basic Express server setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Inventory API is running');
+})
+
+app.listen(3012, () => console.log('Server running on port 3012'));
