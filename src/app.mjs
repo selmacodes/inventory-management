b5323abc53c091916eb8a8fccbdd9b1b6debebd7
Selma <selma-k@hotmail.se>
2025-11-29 17:31:56 +0100

    Ensure server port is a number

--- src/app.mjs
+++ src/app.mjs
@@ -10,6 +10,8 @@ import supplierRouter from "./routes/supplierRoutes.mjs"
 
 import { setupDatabase } from "./config/setupDatabase.mjs";
 
+const appPort = Number(process.env.PORT);
+
 const app = express();
 app.use(express.json()); // Middleware för att kunna läsa JSON i request body
 
@@ -21,6 +23,6 @@ setupDatabase().catch(err => {
 app.use("/products", productRouter);
 app.use("/suppliers", supplierRouter);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+app.listen(appPort, () => {
+    console.log(`Server running on port ${appPort}`);
 });
